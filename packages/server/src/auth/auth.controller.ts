31e43207bbe152e0675c195346feb1f4f20d717a
Dmytro Nesteruk <dmytro.nesteruk.13@gmail.com>
2023-11-13 23:13:35 +0200

    feature/prisma: implemented prisma service;

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from "@nestjs/common";
 
 import { AuthService } from "./auth.service";
+import { SignUpDTO } from "./dto";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("sign-up")
-	signUp(@Body() dto: { email: string }) {
+	signUp(@Body() dto: SignUpDTO) {
 		return this.authService.signUp(dto);
+	}
+
+	@Post("sign-in")
+	signIn(@Body() dto: { email: string }) {
+		return this.authService.signIn(dto);
 	}
 }
--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -1,12 +1,33 @@
+import * as crypto from "node:crypto";
+
 import { Injectable } from "@nestjs/common";
+
+import { PrismaService } from "../prisma/prisma.service";
+
+import { SignUpDTO } from "./dto";
 
 @Injectable()
 export class AuthService {
-	signUp(dto: { email: string }) {
-		return dto;
+	constructor(private prisma: PrismaService) {}
+
+	async signUp(dto: SignUpDTO) {
+		console.log(crypto);
+
+		const hash = crypto.pbkdf2Sync(dto.password, "salt", 10, 50, "sha512").toString("hex");
+
+		console.log(hash);
+
+		const user = await this.prisma.user.create({ data: { email: dto.email, hash } });
+
+		const response = {
+			email: user.email,
+			id: user.id,
+		};
+
+		return response;
 	}
 
-	signIn() {
-		return { message: "Sign In" };
+	signIn(dto: { email: string }) {
+		return dto;
 	}
 }
